331e3707c1bc99e1521260dfc3d2a9d7bcd260ff
AnandPawanipagar <anand.pawanipagar@innovacle.com>
2022-03-11 18:17:01 +0530

    que 4 is pending

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
-let highscoresCount = 0;
-let lowscoresCount = 0;
-let ans = [];
-let highscores = scores.map((e, i) => {
-  let incArr = scores.slice(0, ++i);
-  let max = Math.max(...incArr);
-  return max;
-});
-let lowscores = scores.map((e, i) => {
-  let incArr = scores.slice(0, ++i);
-  let min = Math.min(...incArr);
-  return min;
-});
-console.log(highscores);
-console.log(lowscores);
-let hsCountArr = highscores.map((e, i) => {
-  if (highscores[i + 1] > highscores[i]) {
-    return ++highscoresCount;
-  } else {
-    return highscoresCount;
+let x1 = 0;
+let v1 = 3;
+let x2 = 4;
+let v2 = 2;
+for (x1; x1 < 20; x1 = x1 + v1) {
+  x1=x1+v1;
+  x2=x2+v2;
+  if(x1=x2){
+    console.log('yes' ,x1,x2)
   }
-});
-let hsCountArrLastVal = hsCountArr.pop();
-ans.push(hsCountArrLastVal);
-
-let lsCountArr = lowscores.map((e, i) => {
-  if (lowscores[i + 1] < lowscores[i]) {
-    return ++lowscoresCount;
-  } else {
-    return lowscoresCount;
-  }
-});
-let lsCountArrLastVal = lsCountArr.pop();
-ans.push(lsCountArrLastVal);
-
-console.log(ans);
+}
